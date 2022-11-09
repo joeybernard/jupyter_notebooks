1f95859cc970b6da646320fa0e8b1ec8a48880e9
Joey Bernard <druid.bernard@gmail.com>
2022-11-09 15:13:43 -0400

    Talk - Done

--- performance_talk/ndarray.py
+++ performance_talk/ndarray.py
@@ -2,11 +2,11 @@ import time
 import math
 import numpy
 
-t0 = time.time()
 
 size = 10000000
 A = numpy.arange(size)
 B = numpy.arange(size)
+t0 = time.time()
 for i in range(size):
     B[i] = math.tan(A[i])
 
